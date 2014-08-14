9c6b5bbafffc32a7568cfca58b90d38edf29cc56
Meewan <31101990>
2014-08-14 18:16:11 +0200

    first commit

---
+++ www/js/main.js
@@ -0,0 +1,386 @@
+"use strict";
+var socket;
+var sessionId;
+var currentChannel;
+var pseudo;
+init();
+function init()
+{
+    socket = io.connect();
+    socket.on('back',backMessageHandler);
+    window.onbeforeunload = function (){
+        socket.emit('quit', JSON.serialize({data : sessionId}));
+    };
+}
+
+function submitForm()
+{
+    var login = document.getElementById('login').value;
+    var password = document.getElementById('password').value;
+    socket.emit('submitconnect', JSON.stringify({
+        "user" : login,
+        "password" : password
+    }));
+}
+
+function main(data)
+{
+    //creation des callbacks
+    socket.on('connected', connectedMessageHandler);
+    socket.on('motd', motdMessageHandler);
+    socket.on('names', namesMessageHandler);
+    socket.on('topic', topicMessageHandler);
+    socket.on('join', joinMessageHandler);
+    socket.on('part', partMessageHandler);
+    socket.on('quit', quitMessageHandler);
+    socket.on('kick', kickMessageHandler);
+    socket.on('kill', killMessageHandler);
+    socket.on('message', messageMessageHandler);
+    socket.on('notice', noticeMessageHandler);
+    socket.on('ping', pingMessageHandler);
+    socket.on('pm', pmMessageHandler);
+    socket.on('nick', nickMessageHandler);
+    socket.on('+mode', addModeMessageHandler);
+    socket.on('-mode', remModeMessageHandler);
+    socket.on('whois', whoisMessageHandler);
+    socket.on('error', ircErrorHandler);
+    socket.on('action', actionMessageHandler);
+    socket.on('pvtaction', privateActionMessageHandler);
+    pseudo = data.pseudo;
+    for(var i = 0; i < data.channels.length; i++)
+    {
+        currentChannel = data.channels[i];
+        var channel = data.channels[i].replace('#','_');
+        var chanDOM = '<div class="chanWrapper" id="chan' + channel + 'wrapper">';
+            chanDOM += '<div class="chanUsers" id="chan' + channel + 'users">';
+            chanDOM += '</div>';
+            chanDOM += '<div class="chanMain" id="chan' + channel + 'main">';
+                chanDOM += '<div class="chanTopic" id="chan' + channel + 'topic">';
+                chanDOM += '</div>';
+                chanDOM += '<table class="chanData" id="chan' + channel + 'data">';
+                chanDOM += '</table>';
+                chanDOM += '<div class="chanLower" id="chan' + channel + 'lower">';
+                    chanDOM += '<span class="pseudo" >';
+                        chanDOM += pseudo;
+                    chanDOM += '</span>';
+                    chanDOM += '<input class="chanInput" type="text" id="chan' + channel + 'input" />';
+                    chanDOM += '<input type="button" value="send" onclick="send(\'' + channel + '\')" />';
+                chanDOM += '</div>';
+            chanDOM += '</div>';
+        chanDOM += '</div>';
+        document.getElementById('irc').innerHTML += chanDOM;
+        if(i !== 0)
+        {
+            document.getElementById('chan' + data.channels[i-1].replace('#','_') + 'wrapper').style.display = 'none';
+        }
+        getTopic(data.channels[i]);
+        getNames(data.channels[i]);
+    }
+}
+
+function send(chan)
+{
+    var channel = chan.replace('_','#');
+    socket.emit('message', JSON.stringify({
+        sessionId : sessionId,
+        channel : channel,
+        message : document.getElementById('chan' + chan + 'input').value
+    }));
+    document.getElementById('chan' + chan + 'input').value = '';
+}
+
+
+function getTopic(channel)
+{
+    socket.emit('message', JSON.stringify({
+        sessionId : sessionId,
+        channel : channel,
+        command : 'topic'
+    }));
+}
+
+function getNames(channel)
+{
+    socket.emit('message', JSON.stringify({
+        sessionId : sessionId,
+        channel : channel,
+        command : 'names'
+    }));
+    console.log('names');
+}
+
+function htmlSpecialChar(text)
+{
+    var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+///////////////////////////////////Display Logic////////////////////////////////////////////////////////////////////////
+/**
+ * print a message in a channel or a list of channels, if chan is "*" print the message in all channels
+ * @param source
+ * @param chan
+ * @param message
+ * @param date
+ * @param type
+ */
+function printMessage(date, src, chan, msg, type)
+{
+    var channel = chan.replace('#','_');
+    var sDate = new Date(date);
+    var hdate = (sDate.getHours() <10 ? '[0': '[' ) +sDate.getHours() + (sDate.getMinutes() <10 ? ':0': ':' ) + sDate.getMinutes() + (sDate.getSeconds() <10 ? ':0': ':' ) + sDate.getSeconds() + ']';
+    var source = htmlSpecialChar(src);
+    var message = htmlSpecialChar(msg);
+    var line = '<tr class="message ' + type + '">';
+        line += '<td class="messageTime">';
+            line +=  hdate;
+        line += '</td>';
+        line += '<td class="messageSource">';
+            line += source;
+        line += '</td>';
+        line += '<td class="messageData">';
+            line += message;
+        line += '</td>';
+    line += '</tr>';
+    document.getElementById('chan' + channel + 'data').innerHTML += line;
+}
+/**
+ * redraw the userlist for a channel
+ * @param chan
+ * @param users
+ */
+function displayUserOnChan(chan, users)
+{
+    var channel = chan.replace('#','_');
+    var line = '';
+    for(var user in users)
+    {
+        line += '<div id="userChan' +channel + user +'" class="user">' + users[user] + user + '</div>';
+
+    }
+    document.getElementById('chan' + channel + 'users').innerHTML = line;
+
+}
+/**
+ * add (if action == add) or remove (if action == remove) or change nick (if action == nick) a user from the chan or an
+ * array of chans
+ * @param action
+ * @param chan
+ * @param usr
+ * @param newName
+ */
+function updateUserOnChan(action, chan, usr, newName)
+{
+    if(!(chan instanceof Array))
+    {
+        chan = [chan];
+    }
+    var user = htmlSpecialChar(usr);
+    var chanUser = null;
+    for(var i = 0; i < chan.length; i++)
+    {
+        var channel = chan[i].replace('#','_');
+        if(action === 'add')
+        {
+            chanUser = document.getElementById('userChan' +channel + user);
+            if (chanUser === null)
+            {
+                document.getElementById('chan' + channel + 'users').innerHTML += '<div id="userChan' + channel + user + '" class="user">' + user + '</div>';
+            }
+            else
+            {
+                chanUser.style.display = 'block';
+            }
+
+        }
+        else if (action === 'remove')
+        {
+            chanUser = document.getElementById('userChan' + channel + user);
+            if (chanUser !== null)
+            {
+                chanUser.style.display = 'none';
+            }
+
+        }
+        else if(action === 'nick')
+        {
+            chanUser = document.getElementById('userChan' +channel + user);
+            if(chanUser !== null)
+            {
+                chanUser.innerHTML = newName;
+                chanUser.id = 'userChan' +channel + newName;
+            }
+
+        }
+        chanUser = null;
+    }
+}
+/**
+ * Update the topic for a channel
+ * @param chan
+ * @param topic
+ */
+function updateTopicOnChan(chan, topic)
+{
+    var channel = chan.replace('#','_');
+    document.getElementById('chan' + channel + 'topic').innerHTML = '<span class="topicWord" >' + htmlSpecialChar(topic) + '</span>';
+}
+//////////////////////////////////socket handler////////////////////////////////////////////////////////////////////////
+function backMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    if(data.sessionId)
+    {
+        sessionId = data.sessionId;
+        document.getElementById('loginForm').style.display = 'none';
+        main(data);
+    }
+    else if(data.error)
+    {
+        if(data.error = 'wrongAuth')
+        {
+            document.getElementById('password').value = '';
+        }
+    }
+}
+
+/**
+ * fired when the server is connected to irc
+ * @param serialized
+ */
+function connectedMessageHandler(serialized)
+{
+    //do nothing
+}
+
+/**
+ * fired when a MOTD is received from irc
+ * @param serialized
+ */
+function motdMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    printMessage(data.date, 'MOTD', currentChannel, data.data, 'motdMessage');
+}
+
+function namesMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    displayUserOnChan(data.channel, data.data);
+}
+
+function topicMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    updateTopicOnChan(data.channel, data.data);
+}
+
+function joinMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    updateUserOnChan('add',data.channel, data.from);
+    printMessage(data.date, '--->', data.channel,  data.from + ' joined the chan ', 'joinMessage');
+}
+
+function partMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    updateUserOnChan('remove',data.channel, data.from);
+    printMessage(data.date, '|<---', data.channel, data.from + ' parted the chan '+ (data.data ? data.data : ''), 'partMessage');
+}
+
+function quitMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    updateUserOnChan('remove', data.channels,data.from);
+    printMessage(data.date, '<---', data.channels, data.from + ' quited the network '+ (data.data ? data.data : ''), 'quitMessage');
+}
+
+function kickMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    updateUserOnChan('remove',data.channel, data.target);
+    printMessage(data.date, '|<---', data.channel, data.target +' was kick by ' + data.by + (data.data ? 'reason : ' + data.data : ''), 'kickMessage');
+}
+
+function killMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    updateUserOnChan('remove', data.channels,data.target);
+    printMessage(data.date, '<---', data.channels, data.target + ' quited the network '+ (data.data ? data.data : ''), 'killMessage');
+}
+
+function messageMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    printMessage(data.date, data.from, data.channel, data.data, 'plainMessage');
+}
+
+function actionMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    printMessage(data.date, data.from, data.channel, data.data, 'actionMessage');
+}
+
+function noticeMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    printMessage(data.date, data.from, (data.target ? data.target : currentChannel), data.data, 'noticeMessage');
+}
+
+function pingMessageHandler(serialized)
+{
+    //do nothing
+}
+
+function nickMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    updateUserOnChan('nick', data.to, data.from, data.data);
+    printMessage(data.date, '=-=' , data.to , data.from + ' is now known as ' + data.data, 'nickMessage');
+}
+
+function pmMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    printMessage(data.date, data.from, data.channel, data.data, 'pmMessage');
+}
+
+function privateActionMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    printMessage(data.date, data.from, data.channel, data.data, 'pmAction');
+}
+
+function addModeMessageHandler(serialized)
+{
+    //todo modes
+}
+
+function remModeMessageHandler(serialized)
+{
+    //todo modes
+}
+
+function whoisMessageHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    printMessage(data.date, '=-=', currentChannel,  + data.data.nick + ' ' + data.data.user + data.data.host, 'whois');
+}
+
+function ircErrorHandler(serialized)
+{
+    var data = JSON.parse(serialized);
+    printMessage(data.date, 'ERROR', currentChannel, data.data.command, 'error');
+}
+function errorMessageHandler(serialized)
+{
+    /*var data = JSON.parse(serialized);
+    printMessage(data.date, ERROR, currentChannel, data.data.command, 'error');*/
+}
---
+++ www/server.js
@@ -0,0 +1,511 @@
+var config ='';
+var static = require('node-static');
+var http = require('http');
+var crypto = require('crypto');
+var file = new(static.Server)();
+var sessions = new Array();
+var static = require('node-static');
+var http = require('http');
+var file = new(static.Server)();
+var irc = require('irc');
+var redis = require('redis');
+var redisClient;
+var pseudo;
+var client; //client irc
+//loading configuration
+require('fs').readFile('config.json', 'utf-8', function(err, data)
+    {
+        config = JSON.parse(data);
+        pseudo = config.irc.user;
+        createServer();
+    });
+
+function createServer()
+{
+    startRedis(redis);
+    startIRC(irc);
+    var app = http.createServer(function (req, res) {
+        file.serve(req, res);
+    }).listen(config.web.port);
+
+
+///////////////////////////////////the connection to the webclients logic///////////////////////////////////////////////
+    io = require('socket.io').listen(app);
+    io.sockets.on('connection', function (socket)
+    {
+        socket.on('submitconnect', function (serialized)
+        {
+            data = JSON.parse(serialized);
+            if (data.user.toLowerCase() === config.web.login.toLowerCase() && data.password === config.web.password)
+            {
+                //on genere un token de session a partir de l'ip et de bruit (pas sécurisé d'un point de vue chiffrement mais suffisant)
+                var sessionToken = generateSessionToken(socket.handshake.address);
+                sessions[sessionToken] = {
+                    "connected": true
+                };
+                console.log(sessionToken);
+                socket.room = 'logged';
+                socket.join(socket.room);
+                socket.emit('back', JSON.stringify({
+                    "sessionId": sessionToken,
+                    channels: config.irc.channels,
+                    pseudo : pseudo
+                }));
+                userPseudoManagement();
+            }
+            else
+            {
+                socket.emit('back',JSON.stringify(
+                {
+                    "error": 'wrongAuth'
+                }));
+            }
+        });
+
+        socket.on('message', function(serialized)
+        {
+            var data = JSON.parse(serialized);
+            if(data.sessionId !== undefined && data.sessionId !== null && sessions[data.sessionId].connected === true )
+            {
+                if(data.command === 'names')
+                {
+                    names(data.channel);
+                }
+                else if(data.command === "topic")
+                {
+                    topic(data.channel);
+                }
+                else
+                {
+                    messageListenner(pseudo, data.channel, data.message, null);
+                    say(data.channel, data.message);
+                }
+            }
+        });
+
+        socket.on('quit', function (serialized) {
+            var data = JSON.parse(serialized);
+            //TODO trouver un moyen pour deconecter les gens en cas de timeout
+            //var session = sessions[data.sessionId];
+            sessions = deleteFromArray(sessions, data.sessionId);
+            if (sessions.length == 0)
+            {
+                nick(pseudo +'[away]');
+            }
+
+
+        });
+    });
+}
+/**
+ * Generate a session token in order to emulate php sessions
+ * @param entropy
+ * @returns String the sessionToken
+ */
+function generateSessionToken(entropy)
+{
+    var hash;
+    if(entropy == undefined || entropy == null)
+    {
+        entropy = '';
+    }
+    var totalSeed = '' + entropy +  + Math.random() ;
+    hash = crypto.createHash('md5').update(totalSeed).digest('hex');
+
+    return hash;
+}
+
+function userPseudoManagement()
+{
+    if(pseudo.substring(pseudo.lastIndexOf("[")) === '[away]')
+    {
+        nick(pseudo.substring(0,pseudo.lastIndexOf("[")-1));
+    }
+}
+////////////////////////////////////the connection to irc logic/////////////////////////////////////////////////////////
+function startIRC(irc)
+{
+    client = new irc.Client(config.irc.server, pseudo,
+        {
+            port: config.irc.port,
+            channels: config.irc.channels,
+            userName: config.irc.userName,
+            realName: config.irc.clientName,
+            debug: false
+        });
+
+    client.addListener('registered', function (message)
+    {
+        var data =
+        {
+            type : 'connected',
+            date : (new Date().getTime()),
+            data: message
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('motd', function (message)
+    {
+        var data =
+        {
+            type : 'motd',
+            date : (new Date().getTime()),
+            data: message
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('names', function (chan, nicks)
+    {
+        var data =
+        {
+            type : 'names',
+            date : (new Date().getTime()),
+            channel : chan,
+            data : nicks
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('topic', function (channel, topic, nick, message)
+    {
+        var data =
+        {
+            type : 'topic',
+            date : (new Date().getTime()),
+            channel : channel,
+            from : nick,
+            data :topic
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('join', function (channel, nick, message)
+    {
+        var data =
+        {
+            type : 'join',
+            date : (new Date().getTime()),
+            channel : channel,
+            from :nick
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('part', function (channel, nick, reason, message)
+    {
+        var data =
+        {
+            type : 'part',
+            date : (new Date().getTime()),
+            channel : channel,
+            from : nick,
+            data :  reason
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('quit', function (channels, nick, reason, message)
+    {
+        var data =
+        {
+            type : 'quit',
+            date : (new Date().getTime()),
+            channels : channels,
+            from : nick,
+            data :  reason
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('kick', function (channel, nick, by, reason, message)
+    {
+        var data =
+        {
+            type : 'kick',
+            date : (new Date().getTime()),
+            channel : channel,
+            target : nick,
+            from : by,
+            data :  reason
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('kill', function (nick, reason, channels, message)
+    {
+        var data =
+        {
+            type : 'kill',
+            date : (new Date().getTime()),
+            channels : channels,
+            target : nick,
+            data :  reason
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('message#', messageListenner);
+
+    client.addListener('notice', function (nick, to, text, message)
+    {
+        var data =
+        {
+            type : 'notice',
+            date : (new Date().getTime()),
+            from : nick,
+            target : to,
+            data :text
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('ping', function (server)
+    {
+        var data =
+        {
+            type : 'ping',
+            date : (new Date().getTime()),
+            data :server
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('pm', function (nick, text, message)
+    {
+        var data =
+        {
+            type : 'pm',
+            date : (new Date().getTime()),
+            from : nick,
+            data :text
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('nick', function (oldnick, newnick, channels, message)
+    {
+        if (oldnick === pseudo)
+        {
+            pseudo = newnick;
+        }
+        var data =
+        {
+            type : 'nick',
+            date : (new Date().getTime()),
+            from : oldnick,
+            to : channels,
+            data :newnick
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('+mode', function (channel, by, mode, argument, message)
+    {
+        var data =
+        {
+            type : '+mode',
+            date : (new Date().getTime()),
+            from : by,
+            to : argument,
+            channel : channel,
+            data :mode
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('-mode', function (channel, by, mode, argument, message)
+    {
+        var data =
+        {
+            type : '-mode',
+            date : (new Date().getTime()),
+            from : by,
+            to : argument,
+            channel : channel,
+            data :mode
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('whois', function (whois)
+    {
+        var data =
+        {
+            type : 'whois',
+            date : (new Date().getTime()),
+            to : whois.nick,
+            data :whois
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('error', function (error)
+    {
+        var data =
+        {
+            type : 'error',
+            date : (new Date().getTime()),
+            data :error
+        };
+        storeInRedis(data);
+        console.log(JSON.stringify(data));
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+
+    client.addListener('action', function (from, channel, message)
+    {
+        var data =
+        {
+            type : (channel === pseudo ? 'pvtaction' :'action'),
+            date : (new Date().getTime()),
+            from : from,
+            channel : channel,
+            data :message
+        };
+        storeInRedis(data);
+        io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+    });
+}
+
+function messageListenner(from, channel, text, message)
+{
+    var data =
+    {
+        type : 'message',
+        date : (new Date().getTime()),
+        from : from,
+        channel : channel,
+        data :text
+    };
+    storeInRedis(data);
+    io.sockets.in('logged').emit(data.type, JSON.stringify(data));
+}
+//-----------------------------actions on IRC------------------------------------------------------------------//
+function nick(newNick)
+{
+    client.send("NICK", newNick);
+}
+function names(channel)
+{
+    client.send("NAMES", channel);
+}
+function topic(channel, topic)
+{
+    if(topic !== null && topic !== undefined)
+    {
+        client.send('TOPIC', channel, topic);
+    }
+    else
+    {
+        client.send('TOPIC', channel);
+    }
+
+}
+function say(target, message)
+{
+    client.say(target, message);
+}
+function join(channel)
+{
+    client.join(channel);
+}
+
+function part(channel, reason)
+{
+    client.part(channel,(reason === null || reason === undefined ? '' : reason));
+}
+
+function action(channel, text)
+{
+    client.action(channel, text);
+}
+
+function whois(target)
+{
+    client.whois(target);
+}
+//-----------------------------listeners------------------------------------------------------------------------//
+///////////////////////////////////redis logic//////////////////////////////////////////////////////////////////////////
+function startRedis(redis)
+{
+    redisClient = redis.createClient();
+    redisClient.on("error", function (err)
+    {
+        console.log("Error " + err);
+    });
+}
+
+function storeInRedis(data)
+{
+    if(data.channels)
+    {
+        console.log('channels');
+        for(var i = 0; i < data.channels.length; i++)
+        {
+            storeInRedisChannel(data, data.channels[i]);
+        }
+    }
+    else if(data.type === 'pm' || data.type === 'pvtaction')
+    {
+        redisClient.lpush('pvt_' + data.from.toLowerCase(), JSON.stringify(data));
+    }
+    else if(data.channel)
+    {console.log('channel');
+        storeInRedisChannel(data, data.channel);
+    }
+    else if (data.type === 'motd')
+    {
+        console.log('motd');
+        redisClient.lpush('motd', JSON.stringify(data));
+    }
+    else if(data.type !== 'ping')
+    {console.log('!ping');
+        redisClient.lpush('client', JSON.stringify(data));
+    }
+}
+
+function storeInRedisChannel(data, channel)
+{
+    redisClient.lpush('chan_' + channel.replace('#',''), JSON.stringify(data));
+}
+
+///////////////////////////////////the generic logic////////////////////////////////////////////////////////////////////
+
+
+/**
+ * method that delete an element from an array
+ * !!!!!VERY SLOW DO NOT USE IF THERE IS AN OTHER WAY!!!!!!!!!!
+ * @param position
+ * @param array
+ */
+function deleteFromArray(array, position)
+{
+    var tmpArray = new Array();
+    for (var i = 0; i < array.length; i++)
+    {
+        if(array[i] !== array[position])
+        {
+            tmpArray[i] = array[i];
+        }
+    }
+    return tmpArray;
+}
+
